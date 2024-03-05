2ca9332b411e7cb2c470169a65dc235768c8e7f7
Amaan Ansari <95982010+Amaaan09@users.noreply.github.com>
2024-03-05 17:11:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return {'message': 'Welcome to the Flask API!'}
+    return {'message': 'Welcome to the Jungle!'}
 
 @app.route('/<name>')
 def hello_name(name):
